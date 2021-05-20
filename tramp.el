bed85a2b8473a2b7fec623f5ecde178921c5b689
nomad@wund <nomad@neuronfarm.net>
2021-05-20 16:08:35 +0200

    tramp.el

--- tramp.el
+++ tramp.el
@@ -5,3 +5,7 @@
 (add-to-list 'tramp-connection-properties
              (list (regexp-quote "/ssh:root@gobt:")
                    "remote-shell" "/usr/local/bin/bash"))
+
+(add-to-list 'tramp-connection-properties
+             (list (regexp-quote "/ssh:root@bogt")
+                   "remote-shell" "/usr/local/bin/bash"))
