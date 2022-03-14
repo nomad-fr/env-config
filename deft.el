61db912308fd7c565721da6e61abacf854dd24e8
Michel Le Cocq <nomad@neuronfarm.net>
2022-03-14 08:40:04 +0100

    extension org-md / commentaires

--- deft.el
+++ deft.el
@@ -1,14 +1,48 @@
+
+;; [C-c C-c] clear the filter
+;; [C-c C-n] quickly create a new note
+;; [C-c C-m] for a filename prompt
+;; [C-c C-r] rename file
+;; [C-c C-d] delete file
+;; [C-c C-q] quit deft.
+;; [C-c C-g] refresh the file browser using the current filter string.
+;; [C-c C-t] will toggle between incremental and regexp search modes.
+;;           Regexp search mode is indicated by an “R” in the mode line.
+;; [C-c C-a] archived
+;;           Files will be moved to deft-archive-directory, a directory named archive within your deft-directory by default.
+;; [C-o]     open a file in another window, without switching to the other window.
+;; [C-u C-o] open the file in another window and switch to that window.
+
+;; For more advanced editing operations, you can also edit the filter string in the minibuffer by pressing C-c C-l. While in the minibuffer, the history of previous edits can be cycled through by pressing M-p and M-n.
+
+;; key to launch deft
+;; (global-set-key (kbd "C-c d") 'deft)
+;; (global-set-key (kbd "<F8>") 'deft)
+(global-set-key [f8] 'deft)
+
 (require 'deft)
 (setq deft-directory "~/clouds/Cloud-Neuronfarm/Notes")
-(setq deft-extension "org")
+
+;; Configure Deft to use .md as the file extension:
+(setq deft-default-extension "md")
+(setq deft-extension "md")
+
+;; To use Org mode in Deft files
 (setq deft-text-mode 'org-mode)
+
 (setq deft-use-filename-as-title t)
+(setq deft-use-filter-string-for-filename t)
+(setq deft-file-naming-rules '((noslash . "-")
+                               (nospace . "-")
+                               (case-fn . downcase)))
+
 ;; (setq deft-auto-save-interval 0)
 
 ;; enable searching for files in subdirectories (those not matching deft-recursive-ignore-dir-regexp)
 (setq deft-recursive t)
 
-(global-set-key [f8] 'deft)
+
+
 
 ;;function to run deft in specified directory
 (defun deft-smv (dir)
