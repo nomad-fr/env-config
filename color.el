9e2641ec685879036e3c404545f3575620cf9154
nomad@wund <nomad@neuronfarm.net>
2022-07-14 07:05:13 +0200

    use theme for color

--- color.el
+++ color.el
@@ -1,47 +1,49 @@
 ;; Color ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(load-theme 'tangotango)
+
 ;; M-x list-color-display to show all available color
 ;; custom colors
 
 (custom-set-faces
- '(ido-first-match ((t (:foreground "#ccff66"))))
- '(ido-incomplete-regexp ((t (:foreground "#ffffff"))))
- '(ido-indicator ((t (:foreground "#ffffff"))))
- '(ido-only-match ((t (:foreground "#ffcc33"))))
- '(ido-subdir ((t (:foreground "#66ff00"))))
+;;  '(ido-first-match ((t (:foreground "#ccff66"))))
+;;  '(ido-incomplete-regexp ((t (:foreground "#ffffff"))))
+;;  '(ido-indicator ((t (:foreground "#ffffff"))))
+;;  '(ido-only-match ((t (:foreground "#ffcc33"))))
+;;  '(ido-subdir ((t (:foreground "#66ff00"))))
  
- '(mode-line ((t (:foreground "#295488" :background "darkorange" :box nil))))
- '(mode-line-inactive ((t (:foreground "darkorange" :background "#295488" :box nil))))
+;;  '(mode-line ((t (:foreground "#295488" :background "darkorange" :box nil))))
+;;  '(mode-line-inactive ((t (:foreground "darkorange" :background "#295488" :box nil))))
 
  '(minimap-active-region-background ((((background dark)) (:background "#181818")) (t (:background "#D3D3D3222222")) "Face for the active region in the minimap.
-             By default, this is only a different background color." :group 'minimap))
+              By default, this is only a different background color." :group 'minimap))
 )
 
-(add-to-list 'default-frame-alist '(foreground-color . "#E0DFDB"))
-(add-to-list 'default-frame-alist '(background-color . "black"))
+;; (add-to-list 'default-frame-alist '(foreground-color . "#E0DFDB"))
+;; (add-to-list 'default-frame-alist '(background-color . "black"))
 
-(set-face-foreground 'font-lock-string-face  "#123467")
-(set-face-foreground 'font-lock-comment-face  "#009380")
-(make-face-italic 'font-lock-comment-face)	
-(set-face-foreground 'font-lock-keyword-face  "orange")
-(make-face-bold 'font-lock-keyword-face)  	
-(set-face-foreground 'font-lock-string-face   "#77bbea") ; bleu cyant
-(set-face-foreground 'font-lock-preprocessor-face "pink")
-(set-face-foreground 'font-lock-constant-face   "green")	
-(set-face-foreground 'font-lock-function-name-face "pink")	
-(set-face-foreground 'font-lock-type-face    "lightblue")
-(make-face-bold 'font-lock-type-face)	  
-(set-face-foreground 'font-lock-variable-name-face "grey")	
-(set-face-foreground 'font-lock-warning-face "red")
-(set-face-underline  'font-lock-warning-face "red")		
-(set-face-foreground 'minibuffer-prompt "orange")	
-(set-background-color "#000000")
+;; (set-face-foreground 'font-lock-string-face  "#123467")
+;; (set-face-foreground 'font-lock-comment-face  "#009380")
+;; (make-face-italic 'font-lock-comment-face)	
+;; (set-face-foreground 'font-lock-keyword-face  "orange")
+;; (make-face-bold 'font-lock-keyword-face)  	
+;; (set-face-foreground 'font-lock-string-face   "#77bbea") ; bleu cyant
+;; (set-face-foreground 'font-lock-preprocessor-face "pink")
+;; (set-face-foreground 'font-lock-constant-face   "green")	
+;; (set-face-foreground 'font-lock-function-name-face "pink")	
+;; (set-face-foreground 'font-lock-type-face    "lightblue")
+;; (make-face-bold 'font-lock-type-face)	  
+;; (set-face-foreground 'font-lock-variable-name-face "grey")	
+;; (set-face-foreground 'font-lock-warning-face "red")
+;; (set-face-underline  'font-lock-warning-face "red")		
+;; (set-face-foreground 'minibuffer-prompt "orange")	
+;; (set-background-color "#000000")
 
 ;; highlight the current line; set a custom face, so we can
 ;; recognize from the normal marking (selection)
 (global-hl-line-mode t) ;; turn it on for all modes by default
 (set-face-background 'hl-line "gray10")
 
-(set-face-attribute 'region nil :background "gray19") ;; couleur de fond des selections
+;;(set-face-attribute 'region nil :background "gray19") ;; couleur de fond des selections
 
 ;; Color end ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
