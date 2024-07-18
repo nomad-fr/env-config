86fcb97e07ab1dcabb7668710df0ddfe04dffe3f
Michel Le Cocq <nomad@neuronfarm.net>
2024-07-18 12:13:51 +0200

    ...

---
+++ sway-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##  1082  apt install sway
+##  1083  apt install waybar
+##  1084  apt install foot
+
+ln -s $(dirname $(pwd)/sway) ~/.config/sway
+ln -s $(dirname $(pwd)/waybar) ~/.config/waybar
